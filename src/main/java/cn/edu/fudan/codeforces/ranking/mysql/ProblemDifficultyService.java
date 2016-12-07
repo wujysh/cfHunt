69c4f0fb575fd251bebd9a361d762432d8bb0474
crazyzhou <969677093@qq.com>
2016-12-07 19:52:44 +0800

    add user service

--- src/main/java/cn/edu/fudan/codeforces/ranking/mysql/ProblemDifficultyService.java
+++ src/main/java/cn/edu/fudan/codeforces/ranking/mysql/ProblemDifficultyService.java
@@ -10,17 +10,24 @@ import org.slf4j.LoggerFactory;
 
 import cn.edu.fudan.codeforces.ranking.entity.Problem;
 
-public class ProblemDifficultyService extends BaseMySQLService{
+public class ProblemDifficultyService extends BaseMySQLService {
 	private static final Logger logger = LoggerFactory.getLogger(ProblemDifficultyService.class.getName());
-	
-	 public List<Problem> listContests(Integer page, Integer max) throws IOException, SQLException {
-		 List<Problem> aList = null;
-		 String sql = "?????"; //TODO
-		 ResultSet selectRes = stmt.executeQuery(sql);
-		 while (selectRes.next()) { //循环输出结果集
-             String username = selectRes.getString("username");
-             String password = selectRes.getString("password");
-         }
-		 return aList; 
-	 }
+
+	public List<Problem> listProblems(Integer page, Integer max) throws IOException, SQLException {
+		List<Problem> aList = null;
+		String sql = ""; //TODO
+		ResultSet selectRes = stmt.executeQuery(sql);
+		while (selectRes.next()) { // 循环输出结果集
+			// String city = selectRes.getString("city");
+			// System.out.println(city);
+		}
+		return aList;
+	}
+
+	public Problem buildProblem(ResultSet selectRes) throws SQLException {
+		Problem problem = new Problem();
+		problem.setIndex(selectRes.getString("numIndex"));
+		problem.setContestId(selectRes.getInt("contestId"));
+		return problem;
+	}
 }
